fd124cedd62a999a84f8e8ace760c7e41025f11e
Jommadev <golam.farhan.khan@jomma.online>
2024-02-16 21:57:52 +0600

    Added maintenance mode

---
+++ src/pages/maintenance/index.js
@@ -0,0 +1,29 @@
+// pages/maintenance.js
+
+import Head from 'next/head';
+
+const Maintenance = () => {
+  return (
+    <div>
+      <Head>
+        <title>Under Maintenance</title>
+        <meta name="description" content="Under Maintenance" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+       <center> <h1>Under Maintenance</h1>
+        <p>Sorry, we`&apos;`re currently undergoing maintenance. Please check back later.</p>
+        </center>
+      </main>
+
+      <footer>
+        {/* You can add a footer if needed */}
+      </footer>
+
+      {/* You can add global styles or scripts here */}
+    </div>
+  );
+};
+
+export default Maintenance;
